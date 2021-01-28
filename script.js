d1347f5e7a30607b581fddd0d6491f9c0eca273b
sankalp-agrawal <sankalp04a@gmail.com>
2021-01-27 23:07:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function getAccel() {
-  DeviceMotionEvent.requestPermission().then(response => {
-      if (response == 'granted') {
-          console.log("accelerometer permission granted");
-          // Do stuff here
+	DeviceMotionEvent.requestPermission().then(response => {
+		if (response == 'granted') {
+			console.log("accelerometer permission granted");
+			
+			// Add a listener to get smartphone orientation in the alpha-beta-gamma axes (units in degrees)
+            window.addEventListener('deviceorientation',(event) => {
+                console.log(event);
+				document.getElementById("myText").value = event.alpha
+            });
+        }
       }
   });
 }
