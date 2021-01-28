3b6d500336e3bec34138ea702ede0c23bf316487
sankalp-agrawal <sankalp04a@gmail.com>
2021-01-28 13:56:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,3 @@ function getAccel() {
 		}
     	});
 }
-
-function getAccel(){
-    DeviceMotionEvent.requestPermission().then(response => {
-        if (response == 'granted') {
-            console.log("accelerometer permission granted");
-            // Do stuff here
-        }
-    });
-}
