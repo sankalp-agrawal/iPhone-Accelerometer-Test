c10c701fc53997d76a3a3b4a47890bb410e52097
sankalp-agrawal <sankalp04a@gmail.com>
2021-01-27 22:44:51 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function getAccel() {
+  DeviceMotionEvent.requestPermission().then(response => {
+      if (response == 'granted') {
+          console.log("accelerometer permission granted");
+          // Do stuff here
+      }
+  });
+}
