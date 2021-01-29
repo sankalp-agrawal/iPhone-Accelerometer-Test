a949156978d2f1e47d150b380976ad712318af59
sankalp-agrawal <sankalp04a@gmail.com>
2021-01-29 14:10:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,18 @@ function recordAccel() {
 function stopRecord() {
 	window.removeEventListener();
 }
+
+function test() {
+	const rows = [
+		["name1", "city1", "some other info"],
+		["name2", "city2", "more info"]
+	];
+
+	let csvContent = "data:text/csv;charset=utf-8,";
+	rows.forEach(function(rowArray) {
+		let row = rowArray.join(",");
+		csvContent += row + "\r\n";
+	});
+	var encodedUri = encodeURI(csvContent);
+	window.open(encodedUri);
+}
