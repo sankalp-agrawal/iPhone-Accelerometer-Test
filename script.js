c73de053baab81ab325c1279c882019b31df01b1
sankalp-agrawal <sankalp04a@gmail.com>
2021-02-03 17:34:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,18 +32,19 @@ function recordAccel() {
 	var e = new Date();
 	var startTime = e.getTime();
 	window.addEventListener('devicemotion', (event) => {
-		var d = new Date();
-		y = Math.round((event.acceleration.y) * 100) / 100
-		var text = document.getElementById("Accel");
-		var time = (d.getTime() - startTime)/1000
-		text.innerHTML = y + " m/s^2" + "Time Passed: " + time + " seconds"
-		console.log(event.acceleration.y + " " + time);
-		console.log([event.acceleration.y, time])
-		var newData = [time, event.acceleration.y]
-		exportData.push(newData);
-		console.log(event)
+	//	var d = new Date();
+	//	y = Math.round((event.acceleration.y) * 100) / 100
+	//	var text = document.getElementById("Accel");
+	//	var time = (d.getTime() - startTime)/1000
+	//	text.innerHTML = y + " m/s^2" + "Time Passed: " + time + " seconds"
+	//	console.log(event.acceleration.y + " " + time);
+	//	console.log([event.acceleration.y, time])
+	//	var newData = [time, event.acceleration.y]
+	//	exportData.push(newData);
+	//	console.log(event)
+		newRecord(event);
 	});
-	console.log("end")
+	console.log("end");
 }
 
 function stopRecord() {
