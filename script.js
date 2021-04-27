72c5af23f92e4aaa768e05cd5016d80828fb4f6d
sankalp-agrawal <sankalp04a@gmail.com>
2021-04-27 13:04:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var x = 1
 var y = 1
 var Start = 1
 var exportData = [
-	["time", "accel_y", "accel_x"],
+	["time", "accel_y"],
 	];
 var newData = [1, 2]
 console.log(exportData);
@@ -20,15 +20,12 @@ function getAccel() {
 function newRecord(t) {
 	var d = new Date();
 	y = Math.round((t.acceleration.y) * 100) / 100
-	x = Math.round((t.acceleration.x) * 100) / 100
 	var text = document.getElementById("Accel");
 	var time = (d.getTime() - window.startTime)/1000
-	text.innerHTML = y + " m/s^2, and " + x + "m/s^2" + "Time Passed: " + time + " seconds"
+	text.innerHTML = y + " m/s^2" + "Time Passed: " + time + " seconds"
 	console.log(t.acceleration.y + " " + time);
 	console.log([t.acceleration.y, time])
-	console.log(t.acceleration.x + " " + time);
-	console.log([t.acceleration.x, time])
-	var newData = [time, t.acceleration.y, t.acceleration.x]
+	var newData = [time, t.acceleration.y]
 	exportData.push(newData);
 	console.log(Start)
 }
@@ -65,15 +62,13 @@ function clear() {
 
 
 function download() {
+
 	let csvContent = "data:text/csv;charset=utf-8,";
 	exportData.forEach(function(rowArray) {
 		let row = rowArray.join(",");
 		csvContent += row + "\r\n";
 	});
 	var encodedUri = encodeURI(csvContent);
-	var datadownload = document.createElement("a");
-	datadownload.href = csvContent;
-	var number = document.getElementById("number").value
-	datadownload.download = String("Experiment_" + number +".csv");
-	datadownload.click();
+	window.open(encodedUri);
+	console.log("downloaded")
 }
